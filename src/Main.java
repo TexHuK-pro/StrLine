59beeccf251556868d59dd9ed4d594937cf28c1b
TexHuK-pro <z97233@ya.ru>
2023-01-07 14:55:24 +0300

    first com

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        while (true) {
+            Scanner in = new Scanner(System.in);
+            System.out.println("Введите название метода или поля через пробел: ");
+            String string = in.nextLine();
+
+            //выход из программы если встретим символ 'q'
+            if (Objects.equals(string, "q")) break;
+
+            System.out.println(creatingNamesForMethodsAndFields(string));
+        }
+    }
+
+    private static String creatingNamesForMethodsAndFields(String string) {
+        String[] arrayFromInputString = string.split(" ");
+        StringBuilder finishedString = new StringBuilder();
+        String preparedWordForString;
+
+        for (int i = 0; i < arrayFromInputString.length; i++) {
+            if (i != 0) {
+                //Замена в каждом слове первую букву на заглавную, кроме первого слова
+                preparedWordForString = arrayFromInputString[i].substring(0, 1).toUpperCase() +
+                        arrayFromInputString[i].substring(1);
+            } else {
+                preparedWordForString = arrayFromInputString[i];
+            }
+            finishedString.append(preparedWordForString);
+        }
+        return finishedString.toString();
+    }
+}
